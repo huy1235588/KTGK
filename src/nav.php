9ce3f32b905d8f9c42f2e727f627a3499ca2e74d
huy1235588 <viettien4869@gmail.com>
2025-01-29 22:57:08 +0700

    Enhance navigation styling and account dropdown; add user information management links and improve hover effects

--- src/nav.php
+++ src/nav.php
@@ -105,15 +105,43 @@ $currentUrl = urlencode($_SERVER['REQUEST_URI']);
                 </li>
 
             <?php endif; ?>
-            <li class="menu-item">
+            <li class="menu-item account">
                 <div class="username">
                     Hello
                     <?= htmlspecialchars($_SESSION['username']) ?>
                 </div>
                 <div class="account-dropdown hs-hidden">
-                    <a href="dangxuat.php?redirect=<?= $currentUrl ?>">
-                        Log out
-                    </a>
+                    <ul class="account-dropdown-list">
+                        <h6 class="account-dropdown-title">
+                            My Information
+                        </h6>
+
+                        <li class="account-dropdown-item">
+                            <a href="profile.php?id=<?= $_SESSION['username'] ?>">
+                                <div class="icon">
+                                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" font-size="20px" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M20 2H8a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2zm-6 2.5a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5zM19 15H9v-.25C9 12.901 11.254 11 14 11s5 1.901 5 3.75V15z"></path>
+                                        <path d="M4 8H2v12c0 1.103.897 2 2 2h12v-2H4V8z"></path>
+                                    </svg>
+                                </div>
+                                Information Management
+                            </a>
+                        </li>
+
+                        <hr class="account-dropdown-divider">
+
+                        <li class="account-dropdown-item">
+                            <a href="dangxuat.php?redirect=<?= $currentUrl ?>">
+                                <div class="icon">
+                                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" font-size="20px" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M16 13v-2H7V8l-5 4 5 4v-3z"></path>
+                                        <path d="M20 3h-9c-1.103 0-2 .897-2 2v4h2V5h9v14h-9v-4H9v4c0 1.103.897 2 2 2h9c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2z"></path>
+                                    </svg>
+                                </div>
+                                Log out
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </li>
         <?php else: ?>
