50fd7a98403ab4ae660495f4bd0c7ca3b4d5f114
huy1235588 <viettien4869@gmail.com>
2025-02-15 21:53:23 +0700

    Fix typo in multiplayer feature icon filename

--- src/product.php
+++ src/product.php
@@ -645,7 +645,7 @@ DongKetNoi($conn);
                                             }
                                             // Nếu có pvp hoặc MMO hoặc multiplayer-> ico_multiplayer.png
                                             else if (stripos($featureLower, 'pvp') !== false || stripos($featureLower, 'mmo') !== false || stripos($featureLower, 'multiplayer') !== false) {
-                                                $featureIcon = 'ico_multilplayer.png';
+                                                $featureIcon = 'ico_multiPlayer.png';
                                             }
 
                                             // Nếu có Co-op -> ico_coop.png
