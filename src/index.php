0fd405b7301671d8eab73e93bed49cc1fd2acfd4
huy1235588 <viettien4869@gmail.com>
2025-02-15 17:31:24 +0700

    Add styling for hot product section: implement layout and hover effects for product links and images

--- src/index.php
+++ src/index.php
@@ -41,6 +41,197 @@
 
         <!-- Content chính -->
         <main class="main">
+            <!-- Session hot game -->
+            <section class="hot-game-section">
+                <h2 class="section-header hot-game-header ">
+                    Featured & Recommended
+                </h2>
+
+                <div class="product hot-game">
+                    <?php
+                    // Kết nối CSDL
+                    $conn = MoKetNoi();
+
+                    // Truy vấn sản phẩm
+                    // Lấy 8 sản phẩm có lượt xem cao nhất
+                    // Không lấy sản phẩm không hoạt động
+                    $sqlProducts = "SELECT * 
+                            FROM products
+                            WHERE isActive = 1
+                            ORDER BY releaseDate DESC
+                            LIMIT 8";
+                    // Thực thi truy vấn
+                    $products = $conn->query($sqlProducts);
+
+                    // Thêm sản phẩm vào productIdList
+
+                    while ($product = $products->fetch_assoc()) {
+                        $productIdList[] = $product['id'];
+                    }
+                    ?>
+
+                    <div class="swiper swiper-hot-game">
+                        <div class="swiper-wrapper">
+                            <?php if (!empty($products)): ?>
+                                <?php foreach ($products as $product): ?>
+                                    <?php
+                                    // Kết nối CSDL
+                                    $conn = MoKetNoi();
+
+                                    // Truy vấn ảnh sản phẩm
+                                    $sqlScreenshots = "SELECT * 
+                                                    FROM product_screenshots
+                                                    WHERE product_id = '" . $product['id'] . "'
+                                                    LIMIT 4";
+                                    // Thực thi truy vấn
+                                    $screenshots = $conn->query($sqlScreenshots);
+
+                                    // Đóng kết nối
+                                    DongKetNoi($conn);
+                                    ?>
+
+                                    <div class="swiper-slide">
+                                        <div class="product-item-hot-game"
+                                            data-id="<?= htmlspecialchars($product['id']) ?>">
+
+                                            <a href="product.php?id=<?= htmlspecialchars($product['id']) ?>" class="product-link">
+                                                <!-- Image -->
+                                                <div class="product-img-container">
+                                                    <!-- Ảnh -->
+                                                    <?php if (!empty($screenshots)): ?>
+                                                        <?php foreach ($screenshots as $screenshot): ?>
+                                                            <img class="product-img"
+                                                                src="<?= htmlspecialchars($screenshot['screenshot']) ?>"
+                                                                alt="<?= htmlspecialchars($product['title']) ?>"
+                                                                loading="lazy" />
+                                                        <?php endforeach; ?>
+                                                    <?php endif; ?>
+
+                                                    <img class="product-img product-img-header active"
+                                                        src="<?= htmlspecialchars($product['headerImage']) ?>"
+                                                        alt="<?= htmlspecialchars($product['title']) ?>"
+                                                        loading="lazy" />
+                                                </div>
+
+                                                <!-- Product info -->
+                                                <div class="product-info">
+                                                    <!-- Title -->
+                                                    <h3 class="product-title">
+                                                        <?= htmlspecialchars(($product['title'])) ?>
+                                                    </h3>
+
+                                                    <!-- Screenshots -->
+                                                    <div class="screenshots">
+                                                        <?php if (!empty($screenshots)): ?>
+                                                            <?php foreach ($screenshots as $screenshot): ?>
+                                                                <img class="screenshot"
+                                                                    src="<?= htmlspecialchars($screenshot['screenshot']) ?>"
+                                                                    alt="<?= htmlspecialchars($product['title']) ?>"
+                                                                    loading="lazy" />
+                                                            <?php endforeach; ?>
+                                                        <?php endif; ?>
+                                                    </div>
+
+                                                    <!-- Description -->
+                                                    <p class="description">
+                                                        <?= htmlspecialchars($product['description']) ?>
+                                                    </p>
+
+                                                    <!-- Price -->
+                                                    <div class="price-container">
+                                                        <div class="price">
+                                                            <span class="price-text ">
+                                                                <?php
+                                                                if ($product['price'] == 0) {
+                                                                    echo "Free";
+                                                                } else {
+                                                                    echo "$" . number_format(htmlspecialchars($product['price']), 2);
+                                                                }
+                                                                ?>
+                                                            </span>
+                                                            <!-- Origin price -->
+                                                            <span class="original-price">
+                                                                <?php
+                                                                // Tính giá gốc
+                                                                $originPrice = $product['price'] / (1 - $product['discount'] / 100);
+                                                                if ($product['price'] && htmlspecialchars($product['discount']) > 0):
+                                                                ?>
+
+                                                                    <?= number_format($originPrice, 2) ?>
+                                                                <?php endif ?>
+                                                            </span>
+                                                        </div>
+                                                        <!-- Discount -->
+                                                        <?php if ($product['price'] && htmlspecialchars($product['discount']) > 0): ?>
+                                                            <span class="discount">
+                                                                <?php
+                                                                echo "-" .  number_format(htmlspecialchars($product['discount']), 0) . "%";
+                                                                ?>
+                                                            </span>
+                                                        <?php endif ?>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                <?php endforeach; ?>
+                            <?php endif; ?>
+                        </div>
+                        <div class="swiper-button swiper-button-next hot-game-next"></div>
+                        <div class="swiper-button swiper-button-prev hot-game-prev"></div>
+                    </div>
+
+                    <script>
+                        document.addEventListener("DOMContentLoaded", function() {
+                            new Swiper('.swiper-hot-game', {
+                                slidesPerView: 1,
+                                spaceBetween: 10,
+                                navigation: {
+                                    nextEl: '.swiper-button-next.hot-game-next',
+                                    prevEl: '.swiper-button-prev.hot-game-prev',
+                                },
+                                loop: true,
+                            });
+                        });
+
+                        // Khi rê chuột vào ảnh screenshot thì thay ảnh chính
+                        const productLinks = document.querySelectorAll('.hot-game .product-link');
+                        productLinks.forEach(function(link) {
+                            const productImage = link.querySelectorAll('.product-img');
+                            const productImageHeader = link.querySelector('.product-img-header');
+                            const productScreenshots = link.querySelectorAll('.screenshot');
+
+                            // Khi rê chuột vào ảnh screenshot thì thay ảnh chính
+                            productScreenshots.forEach(function(screenshot) {
+                                screenshot.addEventListener('mouseover', function() {
+                                    productImage.forEach(function(img) {
+                                        img.classList.remove('active');
+                                    });
+
+                                    const img = link.querySelector(`.product-img[src="${screenshot.src}"]`);
+                                    img.classList.add('active');
+
+                                    // Ẩn ảnh header
+                                    productImageHeader.style.display = 'none';
+                                });
+
+                                // Khi rê chuột ra khỏi ảnh screenshot thì hiện ảnh chính
+                                screenshot.addEventListener('mouseout', function() {
+                                    productImage.forEach(function(img) {
+                                        img.classList.remove('active');
+                                    });
+
+                                    productImageHeader.classList.add('active');
+
+                                    // Hiện ảnh header
+                                    productImageHeader.style.display = 'block';
+                                });
+                            });
+                        });
+                    </script>
+                </div>
+            </section>
+
             <?php if (!empty($genres)): ?>
                 <?php foreach ($genres as $genre): ?>
                     <?php
@@ -161,7 +352,9 @@
                             <?php endif; ?>
                         </ul>
                         <div class="view-more">
-                            <a href="genre.php?genre=<?= htmlspecialchars($genre['name']) ?>">Xem thêm</a>
+                            <a href="genre.php?genre=<?= htmlspecialchars($genre['name']) ?>">
+                                View more
+                            </a>
                         </div>
                     </section>
                 <?php endforeach; ?>
