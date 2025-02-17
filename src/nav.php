cb49b4bc1846a707720b45580b9fdcb052fe748a
huy1235588 <viettien4869@gmail.com>
2025-02-17 07:18:33 +0700

    Update SVG icons in navigation and clean up PHP code formatting

--- src/nav.php
+++ src/nav.php
@@ -219,8 +219,8 @@ if ($result->num_rows > 0) {
                         <li class="account-dropdown-item">
                             <a href="friends.php">
                                 <div class="icon">
-                                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" font-size="20px" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M12 2C6.486 2 2 6.486 2 12c0 3.865 2.905 7.06 6.646 7.917-.05-.572-.146-1.156-.146-1.75 0-2.761 2.239-5 5-5s5 2.239 5 5c0 .594-.096 1.178-.146 1.75C19.095 19.06 22 15.865 22 12c0-5.514-4.486-10-10-10zm0 16c-2.761 0-5-2.239-5-5 0-.594.096-1.178.146-1.75C4.905 12.94 2 9.745 2 6c0-3.314 2.686-6 6-6s6 2.686 6 6c0 .594-.096 1.178-.146 1.75.05.572.146 1.156.146 1.75 0 2.761-2.239 5-5 5z"></path>
+                                    <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 640 512" font-size="20px" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M192 256c61.9 0 112-50.1 112-112S253.9 32 192 32 80 82.1 80 144s50.1 112 112 112zm76.8 32h-8.3c-20.8 10-43.9 16-68.5 16s-47.6-6-68.5-16h-8.3C51.6 288 0 339.6 0 403.2V432c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48v-28.8c0-63.6-51.6-115.2-115.2-115.2zM480 256c53 0 96-43 96-96s-43-96-96-96-96 43-96 96 43 96 96 96zm48 32h-3.8c-13.9 4.8-28.6 8-44.2 8s-30.3-3.2-44.2-8H432c-20.4 0-39.2 5.9-55.7 15.4 24.4 26.3 39.7 61.2 39.7 99.8v38.4c0 2.2-.5 4.3-.6 6.4H592c26.5 0 48-21.5 48-48 0-61.9-50.1-112-112-112z"></path>
                                     </svg>
                                 </div>
                                 Friends
@@ -260,7 +260,7 @@ if ($result->num_rows > 0) {
                 </div>
             </li>
         <?php else: ?>
-            <?php 
+            <?php
             // Get the current script file name without query parameters
             $currentScript = basename(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
             ?>
